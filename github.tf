b74750823ae9ceb14073d10e0bc0330d41d6c88d
Robbert Broersma <git@robbertbroersma.nl>
2024-02-15 20:19:11 +0100

    build: add nlds-community-blocks repo

--- github.tf
+++ github.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "nl-design-system"
+
+    workspaces {
+      name = "github"
+    }
+  }
+
   required_providers {
     github = {
       source  = "integrations/github"
@@ -56,7 +64,23 @@ resource "github_team" "kernteam-triage" {
   privacy        = "closed"
 }
 
+resource "github_team" "gemeente-den-haag" {
+  name        = "gemeente-den-haag"
+  description = "Gemeente Den Haag"
+  privacy     = "closed"
+}
 
+resource "github_team" "denhaag-acato" {
+  name           = "denhaag-acato"
+  parent_team_id = github_team.gemeente-den-haag.id
+  privacy        = "closed"
+}
+
+resource "github_team" "denhaag-draad" {
+  name           = "denhaag-draad"
+  parent_team_id = github_team.gemeente-den-haag.id
+  privacy        = "closed"
+}
 
 resource "github_repository" "nldesignsystem-nl-storybook" {
   name                        = "nldesignsystem.nl-storybook"
@@ -154,5 +178,98 @@ resource "github_repository_collaborators" "kernteam-triage" {
   team {
     permission = "triage"
     team_id    = github_team.kernteam-triage.slug
+  }
+}
+
+resource "github_repository" "nlds-community-blocks" {
+  name                        = "nlds-community-blocks"
+  description                 = "WordPress Gutenberg Blocks for NL Design System components. Authors of WordPress websites can now use NL Design System components to compose their pages. This repository is maintained by the NL Design System community."
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "wordpress", "wordpress-plugin"]
+}
+
+resource "github_branch_protection" "nlds-community-blocks-main" {
+  repository_id = github_repository.nlds-community-blocks.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build", "install", "lint", "test", "message-check"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_repository_collaborators" "nlds-community-blocks-kernteam-admin" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+}
+
+
+resource "github_repository_collaborators" "nlds-community-blocks-kernteam-maintainer" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+}
+
+resource "github_repository_collaborators" "nlds-community-blocks-kernteam-committer" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+}
+
+resource "github_repository_collaborators" "nlds-community-blocks-kernteam-triage" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+}
+
+resource "github_repository_collaborators" "nlds-community-blocks-denhaag-draad" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "push"
+    team_id    = github_team.denhaag-draad.slug
+  }
+}
+
+resource "github_repository_collaborators" "nlds-community-blocks-denhaag-acato" {
+  repository = github_repository.nlds-community-blocks.name
+
+  team {
+    permission = "push"
+    team_id    = github_team.denhaag-acato.slug
   }
 }
