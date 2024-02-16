3a72e723917521210735cf6b126733cee17af5c1
Robbert Broersma <git@robbertbroersma.nl>
2024-02-16 11:32:32 +0100

    build: add `example` repo

--- github.tf
+++ github.tf
@@ -275,3 +275,62 @@ resource "github_repository_collaborators" "nlds-community-blocks-denhaag-acato"
     team_id    = github_team.denhaag-acato.slug
   }
 }
+
+
+resource "github_repository" "example" {
+  name                        = "example"
+  description                 = "Template for a design system based on the NL Design System architecture"
+  is_template                 = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  homepage_url                = "https://nl-design-system.github.io/example/"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "storybook"]
+
+  pages {
+    source {
+      branch = "gh-pages"
+      path   = "/"
+    }
+  }
+}
+
+resource "github_branch_protection" "example-main" {
+  repository_id = github_repository.example.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build", "install", "lint", "test"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_branch_protection" "example-gh-pages" {
+  repository_id = github_repository.example.node_id
+
+  pattern                 = "gh-pages"
+  enforce_admins          = true
+  allows_deletions        = false
+  required_linear_history = true
+  allows_force_pushes     = false
+}
