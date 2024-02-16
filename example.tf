6d240cc47842c7a57cfc0792742c2e431d85707e
Robbert Broersma <git@robbertbroersma.nl>
2024-02-16 18:12:02 +0100

    refactor: split configuration into separate files

---
+++ example.tf
@@ -0,0 +1,81 @@
+resource "github_repository" "example" {
+  name                        = "example"
+  description                 = "Template for a design system based on the NL Design System architecture"
+  is_template                 = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  homepage_url                = "https://nl-design-system.github.io/example/"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "storybook"]
+
+  pages {
+    source {
+      branch = "gh-pages"
+      path   = "/"
+    }
+  }
+}
+
+resource "github_branch_protection" "example-main" {
+  repository_id = github_repository.example.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build", "install", "lint", "test"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_branch_protection" "example-gh-pages" {
+  repository_id = github_repository.example.node_id
+
+  pattern                 = "gh-pages"
+  enforce_admins          = true
+  allows_deletions        = false
+  required_linear_history = true
+  allows_force_pushes     = false
+}
+
+resource "github_repository_collaborators" "example" {
+  repository = github_repository.example.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+}
---
+++ team.tf
@@ -0,0 +1,68 @@
+resource "github_team" "kernteam" {
+  name        = "kernteam"
+  description = "NL Design System kernteam"
+  privacy     = "closed"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team" "kernteam-admin" {
+  name           = "kernteam-admin"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team" "kernteam-committer" {
+  name           = "kernteam-committer"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+}
+
+resource "github_team" "kernteam-dependabot" {
+  name           = "kernteam-dependabot"
+  description    = "Default reviewers for Dependabot pull requests"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+}
+
+resource "github_team" "kernteam-developers" {
+  name           = "kernteam-developers"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+}
+
+resource "github_team" "kernteam-maintainer" {
+  name           = "kernteam-maintainer"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+}
+
+resource "github_team" "kernteam-triage" {
+  name           = "kernteam-triage"
+  parent_team_id = github_team.kernteam.id
+  privacy        = "closed"
+}
+
+resource "github_team" "gemeente-den-haag" {
+  name        = "gemeente-den-haag"
+  description = "Gemeente Den Haag"
+  privacy     = "closed"
+}
+
+resource "github_team" "denhaag-acato" {
+  name           = "denhaag-acato"
+  parent_team_id = github_team.gemeente-den-haag.id
+  privacy        = "closed"
+}
+
+resource "github_team" "denhaag-draad" {
+  name           = "denhaag-draad"
+  parent_team_id = github_team.gemeente-den-haag.id
+  privacy        = "closed"
+}
