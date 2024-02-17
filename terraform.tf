81af8de2406bbc942ef2995dbc8d18cbe7a1fcc7
Robbert Broersma <git@robbertbroersma.nl>
2024-02-17 13:54:49 +0100

    feat: add terraform repo

---
+++ terraform.tf
@@ -0,0 +1,54 @@
+resource "github_repository" "terraform" {
+  name                        = "terraform"
+  description                 = "Infrastructure as code: we configure GitHub via Terraform configuration files"
+  is_template                 = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+}
+
+resource "github_branch_protection" "terraform-main" {
+  repository_id = github_repository.terraform.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_repository_collaborators" "terraform" {
+  repository = github_repository.terraform.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  # Restrict pushes to infrastructure as code to admins and maintainers
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+}
