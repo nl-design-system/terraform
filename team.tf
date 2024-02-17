033d9fbbf8d8779373037a9eb82218faadb98f00
Robbert Broersma <git@robbertbroersma.nl>
2024-02-17 17:54:36 +0100

    feat: add `themes` repo

--- team.tf
+++ team.tf
@@ -157,3 +157,154 @@ resource "github_team" "rivm" {
   description = "Rijksinstituut voor Volksgezondheid en Milieu (RIVM)"
   privacy     = "closed"
 }
+
+resource "github_team" "gemeente-amsterdam" {
+  name        = "gemeente-amsterdam"
+  description = "Gemeente Amsterdam"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-bodegraven-reeuwijk" {
+  name        = "gemeente-bodegraven-reeuwijk"
+  description = "Gemeente Bodegraven-Reeuwijk"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-borne" {
+  name        = "gemeente-borne"
+  description = "Gemeente Borne"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-buren" {
+  name        = "gemeente-buren"
+  description = "Gemeente Buren"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-drechterland" {
+  name        = "gemeente-drechterland"
+  description = "Gemeente Drechterland"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-duiven" {
+  name        = "gemeente-duiven"
+  description = "Gemeente Duiven"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-enkhuizen" {
+  name        = "gemeente-enkhuizen"
+  description = "Gemeente Enkhuizen"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-groningen" {
+  name        = "gemeente-groningen"
+  description = "Gemeente Groningen"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-haarlem" {
+  name        = "gemeente-haarlem"
+  description = "Gemeente Haarlem"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-hoorn" {
+  name        = "gemeente-hoorn"
+  description = "Gemeente Hoorn"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-horst-aan-de-maas" {
+  name        = "gemeente-horst-aan-de-maas"
+  description = "Gemeente Horst aan de Maas"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-leidschendam-voorburg" {
+  name        = "gemeente-leidschendam-voorburg"
+  description = "Gemeente Leidschendam-Voorburg"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-nijmegen" {
+  name        = "gemeente-nijmegen"
+  description = "Gemeente Nijmegen"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-noordoostpolder" {
+  name        = "gemeente-noordoostpolder"
+  description = "Gemeente Noordoostpolder"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-stedebroec" {
+  name        = "gemeente-stedebroec"
+  description = "Gemeente Stedebroec"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-tilburg" {
+  name        = "gemeente-tilburg"
+  description = "Gemeente Tilburg"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-venray" {
+  name        = "gemeente-venray"
+  description = "Gemeente Venray"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-vught" {
+  name        = "gemeente-vught"
+  description = "Gemeente Vught"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-westervoort" {
+  name        = "gemeente-westervoort"
+  description = "Gemeente Westervoort"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-zevenaar" {
+  name        = "gemeente-zevenaar"
+  description = "Gemeente Zevenaar"
+  privacy     = "closed"
+}
+
+resource "github_team" "gemeente-zwolle" {
+  name        = "gemeente-zwolle"
+  description = "Gemeente Zwolle"
+  privacy     = "closed"
+}
+
+resource "github_team" "rid-de-liemers" {
+  name        = "rid-de-liemers"
+  description = "RID De Liemers: gemeenten Duiven, Westervoort en Zevenaar (Regionaal ICT en Inkoop Dienstencentrum)"
+  privacy     = "closed"
+}
+
+resource "github_team" "minaz" {
+  name        = "minaz"
+  description = "Ministerie van Algemene Zaken"
+  privacy     = "closed"
+}
+
+resource "github_team" "minaz-1overheid" {
+  name           = "minaz-1overheid"
+  description    = "1Overheid"
+  parent_team_id = github_team.minaz.id
+  privacy        = "closed"
+}
+
+resource "github_team" "provincie-zuid-holland" {
+  name        = "provincie-zuid-holland"
+  description = "Provincie Zuid-Holland"
+  privacy     = "closed"
+}
---
+++ themes.tf
@@ -0,0 +1,221 @@
+resource "github_repository" "themes" {
+  name                        = "themes"
+  description                 = "Design Tokens and NL Design System themes for organisations that don't have their own Storybook."
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  homepage_url                = "https://nl-design-system.github.io/themes/"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "storybook"]
+
+  template {
+    include_all_branches = false
+    owner                = "nl-design-system"
+    repository           = "example"
+  }
+
+  pages {
+    source {
+      branch = "gh-pages"
+      path   = "/"
+    }
+  }
+}
+
+resource "github_branch_protection" "themes-main" {
+  repository_id = github_repository.themes.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_status_checks {
+    strict   = false
+    contexts = ["Continuous integration"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_branch_protection" "themes-gh-pages" {
+  repository_id = github_repository.themes.node_id
+
+  pattern                 = "gh-pages"
+  enforce_admins          = true
+  allows_deletions        = false
+  required_linear_history = true
+  allows_force_pushes     = false
+}
+
+resource "github_repository_collaborators" "themes" {
+  repository = github_repository.themes.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-amsterdam.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-bodegraven-reeuwijk.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-bodegraven-reeuwijk.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-borne.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-buren.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-den-haag.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-drechterland.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-duiven.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-enkhuizen.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-groningen.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-haarlem.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-hoorn.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-horst-aan-de-maas.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-leidschendam-voorburg.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-nijmegen.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-noordoostpolder.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-stedebroec.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-tilburg.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-utrecht.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-venray.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-vught.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-westervoort.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-zevenaar.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.gemeente-zwolle.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.rid-de-liemers.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.provincie-zuid-holland.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.minaz-1overheid.slug
+  }
+}
