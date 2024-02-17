385652a855ae260464b1c88e5491cc0851c02087
Robbert Broersma <git@robbertbroersma.nl>
2024-02-17 13:28:04 +0100

    feat: add example-with-angular repo

---
+++ example-with-angular.tf
@@ -0,0 +1,85 @@
+resource "github_repository" "example-with-angular" {
+  name                        = "example-with-angular"
+  description                 = "Example website using NL Design System with Angular"
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  homepage_url                = "http://nl-design-system.github.io/example-with-angular/"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "angular"]
+
+  pages {
+    source {
+      branch = "gh-pages"
+      path   = "/"
+    }
+  }
+}
+
+resource "github_branch_protection" "example-with-angular-main" {
+  repository_id = github_repository.example-with-angular.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_status_checks {
+    strict   = false
+    contexts = ["continuous-integration"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_branch_protection" "example-with-angular-gh-pages" {
+  repository_id = github_repository.example-with-angular.node_id
+
+  pattern                 = "gh-pages"
+  enforce_admins          = true
+  allows_deletions        = false
+  required_linear_history = true
+  allows_force_pushes     = false
+}
+
+resource "github_repository_collaborators" "example-with-angular" {
+  repository = github_repository.example-with-angular.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.frameless.slug
+  }
+}
