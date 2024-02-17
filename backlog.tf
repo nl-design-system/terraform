cd8ca7ed9d0af925dac7a725874b43ba1a6ea950
Robbert Broersma <git@robbertbroersma.nl>
2024-02-17 16:28:24 +0100

    feat: add backlog repo

---
+++ backlog.tf
@@ -0,0 +1,58 @@
+resource "github_repository" "backlog" {
+  name                        = "backlog"
+  description                 = "Central product backlog of the NL Design System."
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = true
+  has_wiki                    = false
+  has_discussions             = true
+  vulnerability_alerts        = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system"]
+}
+
+resource "github_branch_protection" "backlog-main" {
+  repository_id = github_repository.backlog.node_id
+
+  pattern                         = "main"
+  enforce_admins                  = true
+  allows_deletions                = false
+  require_signed_commits          = false
+  required_linear_history         = true
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+  lock_branch                     = false
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    restrict_dismissals   = false
+  }
+}
+
+resource "github_repository_collaborators" "backlog" {
+  repository = github_repository.backlog.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+}
