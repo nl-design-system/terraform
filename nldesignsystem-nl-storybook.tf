00c060515e1a6be3d096d197ad8fed61ec2acd63
matijs <matijs@gmail.com>
2024-02-21 16:30:14 +0100

    feat: allow automatic merging of branches

--- nldesignsystem-nl-storybook.tf
+++ nldesignsystem-nl-storybook.tf
@@ -4,6 +4,7 @@ resource "github_repository" "nldesignsystem-nl-storybook" {
   allow_merge_commit          = false
   allow_rebase_merge          = true
   allow_squash_merge          = true
+  allow_auto_merge            = true
   delete_branch_on_merge      = true
   has_issues                  = true
   has_downloads               = false
