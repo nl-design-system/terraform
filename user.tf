470d4413ac8d3673b35ad91011b20ceb0dd99f56
Robbert Broersma <git@robbertbroersma.nl>
2024-02-26 18:02:10 +0100

    feat: add kernteam users

--- user.tf
+++ user.tf
@@ -1,3 +1,35 @@
+data "github_user" "hidde" {
+  username = "Hidde"
+}
+
+data "github_user" "jeffreylauwers" {
+  username = "jeffreylauwers"
+}
+
+data "github_user" "matijs" {
+  username = "matijs"
+}
+
 data "github_user" "nl-design-system-ci" {
   username = "nl-design-system-ci"
+}
+
+data "github_user" "peter-berrevoets" {
+  username = "Peter-Berrevoets"
+}
+
+data "github_user" "renatenlds" {
+  username = "RenateNLDS"
+}
+
+data "github_user" "rianrietveld" {
+  username = "rianrietveld"
+}
+
+data "github_user" "robbert" {
+  username = "Robbert"
+}
+
+data "github_user" "yolijn" {
+  username = "Yolijn"
 }
