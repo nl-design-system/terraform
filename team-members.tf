f52cbf490926eb6d64fd27eff24ebc132b93fd00
Robbert Broersma <git@robbertbroersma.nl>
2024-04-03 17:20:43 +0200

    feat: use Ali as Frameless maintainer
    Robbert and Yolijn had conflicting roles: maintainer for kernteam and push for frameless

--- team-members.tf
+++ team-members.tf
@@ -1,12 +1,19 @@
 resource "github_team_members" "frameless-maintainer" {
   team_id = github_team.frameless-maintainer.id
 
-  members {
-    username = data.github_user.yolijn.username
-  }
+  # Disable Yolijn and Robbert from Frameless maintainer team,
+  # while they have conflicting roles with more permissions in nl-design-system/kernteam.
+
+  # members {
+  #   username = data.github_user.yolijn.username
+  # }
+
+  # members {
+  #   username = data.github_user.robbert.username
+  # }
 
   members {
-    username = data.github_user.robbert.username
+    username = data.github_user.ali-kadhim.username
   }
 }
 
