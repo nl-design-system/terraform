106f69f435c30ea8001022bd8f56926d04ff9765
Robbert Broersma <git@robbertbroersma.nl>
2024-08-01 11:08:24 +0200

    chore: allow VNG Services team to contribute to all themes

--- themes.tf
+++ themes.tf
@@ -249,4 +249,9 @@ resource "github_repository_collaborators" "themes" {
     permission = "push"
     team_id    = github_team.voorbeeld-theme-committer.slug
   }
+
+  team {
+    permission = "push"
+    team_id    = github_team.vng-services-committer.slug
+  }
 }
