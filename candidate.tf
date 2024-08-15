d0adb3648f7917b4a801ae953b5e39a146700385
matijs <matijs@gmail.com>
2024-08-15 12:07:28 +0200

    chore: add candidate repo

---
+++ candidate.tf
@@ -0,0 +1,114 @@
+resource "github_repository" "candidate" {
+  name                        = "candidate"
+  description                 = "NL Design System Candidate components"
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_auto_merge            = true
+  delete_branch_on_merge      = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  vulnerability_alerts        = true
+  homepage_url                = "https://nl-design-system.github.io/candidate/"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system", "storybook"]
+
+  template {
+    owner                = "nl-design-system"
+    repository           = "example"
+    include_all_branches = false
+  }
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+
+  pages {
+    build_type = "workflow"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository_ruleset" "candidate-main" {
+  name        = "main"
+  repository  = github_repository.candidate.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/main"]
+      exclude = []
+    }
+  }
+
+  rules {
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+    required_linear_history = true
+
+    required_status_checks {
+      required_check {
+        context = "build"
+      }
+      required_check {
+        context = "install"
+      }
+      required_check {
+        context = "lint"
+      }
+      required_check {
+        context = "test"
+      }
+    }
+  }
+
+  bypass_actors {
+    actor_id    = data.github_user.nl-design-system-ci.id
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+}
+
+resource "github_repository_collaborators" "candidate" {
+  repository = github_repository.candidate.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.slug
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.slug
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.slug
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-dependabot.slug
+  }
+}
