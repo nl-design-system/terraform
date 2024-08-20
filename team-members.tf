40a21837629160222deb7a6c376b7decaee8d609
Yolijn <yolijn@frameless.io>
2024-08-20 10:04:26 +0200

    chore: remove peter-berrevoets

--- team-members.tf
+++ team-members.tf
@@ -151,10 +151,6 @@ resource "github_team_members" "kernteam-triage" {
   }
 
   members {
-    username = data.github_user.peter-berrevoets.username
-  }
-
-  members {
     username = data.github_user.astrid-01.username
   }
 
