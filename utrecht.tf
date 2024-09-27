e066acaea62d17dd1d059f470724445443907945
Remy Parzinski <remy@MacBook-Pro.io-internal.net>
2024-09-27 10:59:18 +0200

    feat(utrecht): add team logius-maintainer

--- utrecht.tf
+++ utrecht.tf
@@ -101,4 +101,9 @@ resource "github_repository_collaborators" "utrecht" {
     permission = "maintain"
     team_id    = github_team.frameless-maintainer.slug
   }
+
+  team {
+    permission = "push"
+    team_id    = github_team.logius-maintainer.slug
+  }
 }
