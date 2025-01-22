0d3b9d252817273447998f676d6bb271b17df550
matijs <matijs@gmail.com>
2025-01-22 13:18:29 +0100

    fix: use community-committer team_id

--- documentatie.tf
+++ documentatie.tf
@@ -133,7 +133,7 @@ resource "github_repository_collaborators" "documentatie" {
 
   team {
     permission = "push"
-    team_id    = "community-committer"
+    team_id    = github_team.community-committer.id
   }
 }
 
