b07473cb0f1564355cdb62f873aba9da37d1b688
matijs <matijs@gmail.com>
2025-01-24 11:29:54 +0100

    fix: reduce churn
    Prevent unnecessary "changes" by adding the "source" block that GitHub
    keeps around invisibly in the background.
    
    See this example from an API call to the pages endpoint of the candidate
    repository. `"source"` is still present even though `"build_type"` is
    set to `"workflow"`.
    
    ```json
    {
      "url":
        "https://api.github.com/repos/nl-design-system/candidate/pages",
        "status": null,
        "cname": null,
        "custom_404": false,
        "html_url": "https://nl-design-system.github.io/candidate/",
        "build_type": "workflow",
        "source": {
          "branch": "main",
          "path": "/"
        },
        "public": true,
        "protected_domain_state": null,
        "pending_domain_unverified_at": null,
        "https_enforced": true
    }
    ```

--- mendix.tf
+++ mendix.tf
@@ -27,6 +27,12 @@ resource "github_repository" "mendix" {
 
   pages {
     build_type = "workflow"
+
+    # A `source` block is only needed when `build_type` is set to `"legacy"`, but because GitHub keeps it around invisibly, we must add it here to prevent churn
+    source {
+      branch = "main"
+      path   = "/"
+    }
   }
 
   lifecycle {
