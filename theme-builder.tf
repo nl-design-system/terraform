74876c0f0fd9015583aee51331449f35f433e930
matijs <matijs@gmail.com>
2025-01-27 12:53:15 +0100

    feat: add theme-builder repo

---
+++ theme-builder.tf
@@ -0,0 +1,85 @@
+resource "github_repository" "theme-builder" {
+  name                        = "theme-builder"
+  description                 = "Theme Builder for NL Design System"
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_auto_merge            = true
+  delete_branch_on_merge      = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = true
+  has_wiki                    = false
+  has_discussions             = true
+  vulnerability_alerts        = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system"]
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository_ruleset" "theme-builder-main" {
+  name        = "main"
+  repository  = github_repository.theme-builder.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/main"]
+      exclude = []
+    }
+  }
+
+  rules {
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+    required_linear_history = true
+  }
+
+  bypass_actors {
+    actor_id    = github_team.kernteam-ci.id
+    actor_type  = "Team"
+    bypass_mode = "always"
+  }
+}
+
+
+resource "github_repository_collaborators" "theme-builder" {
+  repository = github_repository.theme-builder.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.id
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.id
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.expertteam-digitale-toegankelijkheid-committer.id
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.expertteam-digitale-toegankelijkheid-triage.id
+  }
+}
