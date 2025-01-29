2655faa6fa64a03028f0b37a657b297084cb3766
Yolijn <mail@yolijn.com>
2025-01-29 12:33:56 +0100

    feat: add lux projects

--- lux.tf
+++ lux.tf
@@ -8,7 +8,7 @@ resource "github_repository" "lux" {
   delete_branch_on_merge      = true
   has_issues                  = true
   has_downloads               = false
-  has_projects                = false
+  has_projects                = true
   has_wiki                    = false
   vulnerability_alerts        = true
   homepage_url                = "https://nl-design-system.github.io/lux/"
