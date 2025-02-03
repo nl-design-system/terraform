44385a2c530a6a3660db17cae482dd5600f41887
matijs <matijs@gmail.com>
2025-02-03 15:10:48 +0100

    chore: allow Quintor committers to push to themes

--- themes.tf
+++ themes.tf
@@ -264,4 +264,9 @@ resource "github_repository_collaborators" "themes" {
     permission = "push"
     team_id    = github_team.nora-committer.id
   }
+
+  team {
+    permission = "push"
+    team_id    = github_team.quintor-rijkshuisstijl-committer.id
+  }
 }
