232563f85b00f19c10e7322afb252faef7a8e6a2
matijs <matijs@gmail.com>
2025-02-21 09:52:06 +0100

    chore: switch "lux" to rulesets
    - Add "github_repository_ruleset" for the `~DEFAULT_BRANCH` branch
    - Add "github_repository_ruleset" for the `gh-pages` branch
    - Remove "github_branch_protection"

--- lux.tf
+++ lux.tf
@@ -39,50 +39,85 @@ resource "github_branch_default" "lux" {
   branch     = "main"
 }
 
-resource "github_branch_protection" "lux-main" {
-  repository_id = github_repository.lux.node_id
+resource "github_repository_ruleset" "lux-main" {
+  enforcement = "active"
+  name        = "default-branch-protection"
+  repository  = github_repository.lux.name
+  target      = "branch"
 
-  pattern                         = "main"
-  enforce_admins                  = false
-  allows_deletions                = false
-  require_signed_commits          = false
-  required_linear_history         = true
-  require_conversation_resolution = true
-  allows_force_pushes             = false
-  lock_branch                     = false
-
-  restrict_pushes {
-    blocks_creations = false
-    push_allowances = [
-      "/${data.github_user.nl-design-system-ci.username}",
-      "nl-design-system/${github_team.kernteam-maintainer.name}",
-      "nl-design-system/${github_team.logius-committer.name}",
-      "nl-design-system/${github_team.logius-maintainer.name}",
-    ]
+  bypass_actors {
+    actor_id    = github_team.kernteam-ci.id
+    actor_type  = "Team"
+    bypass_mode = "always"
   }
 
-  required_status_checks {
-    strict   = false
-    contexts = ["build", "lint", "test", "UI Tests"]
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
   }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews = true
-    restrict_dismissals   = false
-    pull_request_bypassers = [
-      "/${data.github_user.nl-design-system-ci.username}",
-    ]
+  rules {
+    creation                      = true
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+
+    required_status_checks {
+      do_not_enforce_on_create             = false
+      strict_required_status_checks_policy = false
+
+      required_check {
+        context = "build"
+      }
+      required_check {
+        context = "lint"
+      }
+      required_check {
+        context = "test"
+      }
+      required_check {
+        context = "UI Tests"
+      }
+    }
   }
 }
 
-resource "github_branch_protection" "lux-gh-pages" {
-  repository_id = github_repository.lux.node_id
+resource "github_repository_ruleset" "lux-other" {
+  enforcement = "active"
+  name        = "other-branch-protection"
+  repository  = github_repository.lux.name
+  target      = "branch"
 
-  pattern                 = "gh-pages"
-  enforce_admins          = true
-  allows_deletions        = false
-  required_linear_history = true
-  allows_force_pushes     = false
+  conditions {
+    ref_name {
+      include = [
+        "refs/heads/gh-pages",
+      ]
+      exclude = []
+    }
+  }
+  rules {
+    creation                      = true
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+  }
 }
 
 resource "github_repository_collaborators" "lux" {
