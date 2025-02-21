8ba1e526e4dcfaacf9946fdb33d57d3922f1b842
matijs <matijs@gmail.com>
2025-02-21 15:06:16 +0100

    chore: switch "matomo" to rulesets
    - Add "github_repository_ruleset" for the default branch
    - Remove "github_branch_protection"

--- matomo.tf
+++ matomo.tf
@@ -33,24 +33,37 @@ resource "github_branch_default" "matomo" {
   branch     = "main"
 }
 
-resource "github_branch_protection" "matomo-main" {
-  repository_id = github_repository.matomo.node_id
+resource "github_repository_ruleset" "matomo-main" {
+  enforcement = "active"
+  name        = "default-branch-protection"
+  repository  = github_repository.matomo.name
+  target      = "branch"
 
-  pattern                         = "main"
-  enforce_admins                  = true
-  allows_deletions                = false
-  require_signed_commits          = false
-  required_linear_history         = true
-  require_conversation_resolution = true
-  allows_force_pushes             = false
-  lock_branch                     = false
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews = true
-    restrict_dismissals   = false
+  rules {
+    creation                      = true
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      require_code_owner_review         = true
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
   }
 }
-
 resource "github_repository_collaborators" "matomo" {
   repository = github_repository.matomo.name
 
