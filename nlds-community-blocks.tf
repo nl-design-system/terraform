f3714e76fed0caa39d432177586f9829e03412db
matijs <matijs@gmail.com>
2025-02-25 15:17:56 +0100

    chore: switch "nlds-community-blocks" to rulesets
    - Add "github_repository_ruleset" for main branch
    - Remove "github_branch_protection"

--- nlds-community-blocks.tf
+++ nlds-community-blocks.tf
@@ -25,36 +25,59 @@ resource "github_branch_default" "nlds-community-blocks" {
   branch     = "main"
 }
 
-resource "github_branch_protection" "nlds-community-blocks-main" {
-  repository_id = github_repository.nlds-community-blocks.node_id
+resource "github_repository_ruleset" "nlds-community-blocks-main" {
+  enforcement = "active"
+  name        = "default-branch-protection"
+  repository  = github_repository.nlds-community-blocks.name
+  target      = "branch"
 
-  pattern                         = "main"
-  enforce_admins                  = false
-  allows_deletions                = false
-  require_signed_commits          = false
-  required_linear_history         = true
-  require_conversation_resolution = true
-  allows_force_pushes             = false
-  lock_branch                     = false
-
-  restrict_pushes {
-    blocks_creations = false
-    push_allowances = [
-      "/${data.github_user.nl-design-system-ci.username}",
-    ]
+  bypass_actors {
+    actor_id    = github_team.kernteam-ci.id
+    actor_type  = "Team"
+    bypass_mode = "always"
   }
 
-  required_status_checks {
-    strict   = false
-    contexts = ["build", "install", "lint", "test", "message-check"]
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
   }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews = true
-    restrict_dismissals   = false
-    pull_request_bypassers = [
-      "/${data.github_user.nl-design-system-ci.username}",
-    ]
+  rules {
+    creation                      = true
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      require_code_owner_review         = true
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+
+    required_status_checks {
+      required_check {
+        context = "build"
+      }
+      required_check {
+        context = "install"
+      }
+      required_check {
+        context = "lint"
+      }
+      required_check {
+        context = "test"
+      }
+      required_check {
+        context = "message-check"
+      }
+    }
   }
 }
 
