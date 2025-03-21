78c5e38d2990dab6767a8317222967421bd31310
matijs <matijs@gmail.com>
2025-03-21 16:00:28 +0100

    fix: add * to patterns_allowed

--- github_organization.tf
+++ github_organization.tf
@@ -10,28 +10,28 @@ resource "github_actions_organization_permissions" "nl-design-system" {
     github_owned_allowed = true
     patterns_allowed = [
       # GitHub owned actions
-      "actions/cache@",
-      "actions/checkout@",
-      "actions/configure-pages@",
-      "actions/create-github-app-token@",
-      "actions/deploy-pages@",
-      "actions/download-artifact@",
-      "actions/setup-node@",
-      "actions/upload-artifact@",
-      "actions/upload-pages-artifact@",
+      "actions/cache@*",
+      "actions/checkout@*",
+      "actions/configure-pages@*",
+      "actions/create-github-app-token@*",
+      "actions/deploy-pages@*",
+      "actions/download-artifact@*",
+      "actions/setup-node@*",
+      "actions/upload-artifact@*",
+      "actions/upload-pages-artifact@*",
       # Third party organisation owned actions
-      "changesets/action@",
-      "chromaui/action@",
-      "codecov/codecov-action@",
-      "hashicorp/setup-terraform@",
-      "php-actions/composer@",
-      "pnpm/action-setup@",
+      "changesets/action@*",
+      "chromaui/action@*",
+      "codecov/codecov-action@*",
+      "hashicorp/setup-terraform@*",
+      "php-actions/composer@*",
+      "pnpm/action-setup@*",
       # Third party individually owned actions
-      "amannn/action-semantic-pull-request@",
-      "JamesIves/github-pages-deploy-action@",
-      "marvinpinto/action-automatic-releases@",
-      "wagoid/commitlint-github-action@",
-      "xt0rted/block-autosquash-commits-action@",
+      "amannn/action-semantic-pull-request@*",
+      "JamesIves/github-pages-deploy-action@*",
+      "marvinpinto/action-automatic-releases@*",
+      "wagoid/commitlint-github-action@*",
+      "xt0rted/block-autosquash-commits-action@*",
     ]
     verified_allowed = false
   }
