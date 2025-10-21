dc93f5baed3f81690c4b68fbb0e329e0885e38d1
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-10-21 10:35:19 +0200

    feat: remove from repos that dont have permission yet

--- rvo.tf
+++ rvo.tf
@@ -152,9 +152,4 @@ resource "github_repository_collaborators" "rvo" {
     permission = "maintain"
     team_id    = github_team.rvo-maintainer.id
   }
-
-  team {
-    permission = "push"
-    team_id    = github_team.community-committer.id
-  }
 }
