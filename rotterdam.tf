cfabbd482a7ac82a157697d2b492322908b92400
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-10-21 10:35:19 +0200

    feat: remove from repos that dont have permission yet

--- rotterdam.tf
+++ rotterdam.tf
@@ -172,9 +172,4 @@ resource "github_repository_collaborators" "rotterdam" {
     permission = "maintain"
     team_id    = github_team.frameless-maintainer.id
   }
-
-  team {
-    permission = "push"
-    team_id    = github_team.community-committer.id
-  }
 }
