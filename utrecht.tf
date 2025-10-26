27504325ea4f7528adae60d2a646624a965aa9e9
Robbert Broersma <git@robbertbroersma.nl>
2025-10-26 15:47:52 +0100

    ci: add tag protection for Utrecht

--- utrecht.tf
+++ utrecht.tf
@@ -101,6 +101,36 @@ resource "github_repository_ruleset" "utrecht-main" {
   }
 }
 
+# Only allow the `nl-design-system-ci` user to manage Git tags
+resource "github_repository_ruleset" "utrecht-tag" {
+  enforcement = "active"
+  name        = "tag-protection"
+  repository  = github_repository.utrecht.name
+  target      = "tag"
+
+  bypass_actors {
+    actor_id    = github_team.kernteam-ci.id
+    actor_type  = "Team"
+    bypass_mode = "always"
+  }
+
+  conditions {
+    ref_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation                = true
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = true
+    update                  = true
+  }
+}
+
 resource "github_repository_collaborators" "utrecht" {
   repository = github_repository.utrecht.name
 
