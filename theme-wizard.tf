1a98209c77ba3c4f272ae3f25afda22f8be0b160
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-12-23 11:14:58 +0100

    Disable vercel preview comments on theme-wizard
    Added 'preview_comments' attribute to the vercel_project resource.

--- theme-wizard.tf
+++ theme-wizard.tf
@@ -145,10 +145,11 @@ resource "github_repository_deployment_branch_policy" "theme-wizard-publish-main
 }
 
 resource "vercel_project" "theme-wizard" {
-  name           = "theme-wizard"
-  ignore_command = "[[ $(git log -1 --pretty=%an) == 'dependabot[bot]' ]]"
-  node_version   = "22.x"
-  root_directory = "packages/theme-wizard-website/"
+  name             = "theme-wizard"
+  ignore_command   = "[[ $(git log -1 --pretty=%an) == 'dependabot[bot]' ]]"
+  node_version     = "22.x"
+  root_directory   = "packages/theme-wizard-website/"
+  preview_comments = false
 
 
   git_repository = {
