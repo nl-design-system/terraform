d9418add0afddfec07cb3fcd5e6c6d65312acc56
Yolijn <mail@yolijn.com>
2026-01-14 17:39:11 +0100

    feat: add beheer repository

---
+++ beheer.tf
@@ -0,0 +1,90 @@
+resource "github_repository" "beheer" {
+  name                        = "beheer"
+  description                 = "Issues voor het beheer van de NL Design System infrastructuur en community"
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_auto_merge            = true
+  delete_branch_on_merge      = true
+  has_issues                  = true
+  has_downloads               = false
+  has_projects                = true
+  has_wiki                    = false
+  has_discussions             = true
+  vulnerability_alerts        = true
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system"]
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_branch_default" "beheer" {
+  repository = github_repository.beheer.name
+  branch     = "main"
+}
+
+resource "github_repository_ruleset" "beheer-main" {
+  enforcement = "active"
+  name        = "default-branch-protection"
+  repository  = github_repository.beheer.name
+  target      = "branch"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation                      = false
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+  }
+}
+
+resource "github_repository_collaborators" "beheer" {
+  repository = github_repository.beheer.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.id
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.id
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.id
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.id
+  }
+}
