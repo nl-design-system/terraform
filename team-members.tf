f06d4f746bb1bd4c29046f8981a530db1609b497
Fionn Mac an Bhaird <33222605+FinnWard@users.noreply.github.com>
2026-01-27 16:28:15 +0100

    chore: add UX PO dorine to logius team (#552)

--- team-members.tf
+++ team-members.tf
@@ -339,6 +339,10 @@ resource "github_team_members" "logius-committer" {
   members {
     username = data.github_user.romy-petitjean-logius.username
   }
+
+  members {
+    username = data.github_user.DorineLogius.username
+  }
 }
 
 resource "github_team_members" "logius-maintainer" {
