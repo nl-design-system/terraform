1389dfa37611a0f17537d6a2a8f75386f646ae18
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-03-09 22:20:40 +0100

    chore: remove kernteam-dependabot team
    See: https://github.blog/changelog/2025-04-29-dependabot-reviewers-configuration-option-being-replaced-by-code-owners/

--- denhaag.tf
+++ denhaag.tf
@@ -182,11 +182,6 @@ resource "github_repository_collaborators" "denhaag" {
   }
 
   team {
-    permission = "triage"
-    team_id    = github_team.kernteam-dependabot.id
-  }
-
-  team {
     permission = "admin"
     team_id    = github_team.gemeente-denhaag-admin.id
   }
--- example.tf
+++ example.tf
@@ -115,11 +115,6 @@ resource "github_repository_collaborators" "example" {
   }
 
   team {
-    permission = "triage"
-    team_id    = github_team.kernteam-dependabot.id
-  }
-
-  team {
     permission = "push"
     team_id    = github_team.community-contributor.id
   }
