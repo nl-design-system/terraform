10d4d0adc719aef90b534f83ffa20387253fbff2
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-05-01 12:12:36 +0200

    ci: refactor order of resources to put github resources together

--- candidate.tf
+++ candidate.tf
@@ -131,6 +131,23 @@ resource "github_repository_collaborators" "candidate" {
   }
 }
 
+resource "github_repository_environment" "candidate-publish" {
+  environment       = "Publish"
+  repository        = github_repository.candidate.name
+  can_admins_bypass = false
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_environment_deployment_policy" "candidate-publish-main" {
+  repository     = github_repository.candidate.name
+  environment    = github_repository_environment.candidate-publish.environment
+  branch_pattern = github_branch_default.candidate.branch
+}
+
 resource "vercel_project" "candidate" {
   name                    = github_repository.candidate.name
   output_directory        = "packages/storybook/dist"
@@ -180,21 +197,4 @@ resource "vercel_project" "candidate-storybook-test" {
   vercel_authentication = {
     deployment_type = "none"
   }
-}
-
-resource "github_repository_environment" "candidate-publish" {
-  environment       = "Publish"
-  repository        = github_repository.candidate.name
-  can_admins_bypass = false
-
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = true
-  }
-}
-
-resource "github_repository_environment_deployment_policy" "candidate-publish-main" {
-  repository     = github_repository.candidate.name
-  environment    = github_repository_environment.candidate-publish.environment
-  branch_pattern = github_branch_default.candidate.branch
 }
