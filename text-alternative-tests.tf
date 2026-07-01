d2f064477ccac4114f272a035eb87383a7cbeab5
Robbert Broersma <git@robbertbroersma.nl>
2026-07-01 11:18:53 +0200

    feat: add repo for test suite with many binary files

---
+++ text-alternative-tests.tf
@@ -0,0 +1,202 @@
+resource "github_repository" "text-alternative-tests" {
+  name                        = "text-alternative-tests"
+  description                 = "Testsuite voor toegankelijke tekstalternatieven."
+  allow_merge_commit          = false
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_auto_merge            = true
+  delete_branch_on_merge      = true
+  has_issues                  = true
+  has_projects                = true
+  has_wiki                    = false
+  has_discussions             = true
+  vulnerability_alerts        = true
+  homepage_url                = "https://editor.nl-design-system-community.nl/alt-tekst-wizard"
+  squash_merge_commit_title   = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  topics                      = ["nl-design-system"]
+
+  template {
+    include_all_branches = false
+    owner                = "nl-design-system"
+    repository           = "example"
+  }
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_branch_default" "text-alternative-tests" {
+  branch     = "main"
+  repository = github_repository.text-alternative-tests.name
+}
+
+resource "github_repository_ruleset" "text-alternative-tests-main" {
+  enforcement = "active"
+  name        = "default-branch-protection"
+  repository  = github_repository.text-alternative-tests.name
+  target      = "branch"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation                      = false
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      require_code_owner_review         = true
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+
+    required_status_checks {
+      strict_required_status_checks_policy = false
+
+      required_check {
+        context = "lint"
+      }
+
+      required_check {
+        context = "build"
+      }
+
+      required_check {
+        context = "test"
+      }
+    }
+  }
+}
+
+# Only allow the `nl-design-system-ci` user to manage Git tags
+resource "github_repository_ruleset" "text-alternative-tests-tag" {
+  enforcement = "active"
+  name        = "tag-protection"
+  repository  = github_repository.text-alternative-tests.name
+  target      = "tag"
+
+  bypass_actors {
+    actor_id    = github_team.kernteam-ci.id
+    actor_type  = "Team"
+    bypass_mode = "always"
+  }
+
+  conditions {
+    ref_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation                = true
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = true
+    update                  = true
+  }
+}
+
+resource "github_repository_collaborators" "text-alternative-tests" {
+  repository = github_repository.text-alternative-tests.name
+
+  team {
+    permission = "admin"
+    team_id    = github_team.kernteam-admin.id
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.kernteam-maintainer.id
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.kernteam-committer.id
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.kernteam-triage.id
+  }
+
+  team {
+    permission = "maintain"
+    team_id    = github_team.expertteam-digitale-toegankelijkheid-maintainer.id
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.expertteam-digitale-toegankelijkheid-committer.id
+  }
+
+  team {
+    permission = "triage"
+    team_id    = github_team.expertteam-digitale-toegankelijkheid-triage.id
+  }
+}
+
+resource "github_repository_environment" "text-alternative-tests-publish" {
+  environment       = "publish"
+  repository        = github_repository.text-alternative-tests.name
+  can_admins_bypass = false
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_environment" "text-alternative-tests-issues" {
+  # Environment for GitHub Actions triggered by issues
+  environment = "Issues"
+  repository  = github_repository.text-alternative-tests.name
+}
+
+resource "github_repository_environment_deployment_policy" "text-alternative-tests-publish-main" {
+  repository     = github_repository.text-alternative-tests.name
+  environment    = github_repository_environment.text-alternative-tests-publish.environment
+  branch_pattern = github_branch_default.text-alternative-tests.branch
+}
+
+
+resource "vercel_project" "text-alternative-tests" {
+  name                    = "text-alternative-tests"
+  output_directory        = "packages/website/dist/"
+  build_command           = "pnpm run build"
+  ignore_command          = "[[ $(git log -1 --pretty=%an) == 'dependabot[bot]' ]]"
+  node_version            = "24.x"
+  enable_preview_feedback = false
+
+  git_repository = {
+    type = "github"
+    repo = github_repository.text-alternative-tests.full_name
+  }
+
+  vercel_authentication = {
+    deployment_type = "none"
+  }
+}
+
