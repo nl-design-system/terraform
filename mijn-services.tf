be458459b463761fda81d0e7a27a393a784f787e
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-07-31 12:29:52 +0200

    ci: remove vercel_project configuration that will be handled by vercel.json

--- mijn-services.tf
+++ mijn-services.tf
@@ -185,9 +185,8 @@ resource "github_repository_environment" "mijn-services-maintenance" {
 
 resource "vercel_project" "mijn-services" {
   name                    = github_repository.mijn-services.name
-  output_directory        = "packages/storybook/dist/"
-  ignore_command          = "[[ $(git log -1 --pretty=%an) == 'dependabot[bot]' ]]"
   node_version            = "24.x"
+  root_directory          = "packages/storybook/"
   enable_preview_feedback = false
 
   git_repository = {
